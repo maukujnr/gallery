99a48e3db54843839cff677dad49e23c50161c96
maukujnr <justmauku@gmail.com>
2023-09-26 02:19:27 +0300

    modified server.js file

--- server.js
+++ server.js
@@ -1,56 +1,42 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const multer = require('multer');
 const path = require('path');
 const config = require('./_config');
 
 // Define routes
-let index = require('./routes/index');
-let image = require('./routes/image');
+const index = require('./routes/index');
+const image = require('./routes/image');
 
 // Initializing the app
 const app = express();
 
-// connecting the database
-
-const MONGODB_URI = process.env.MONGODB_URI || config.mongoURI[app.settings.env]
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true  },(err)=>{
-    if (err) {
-        console.log(err)
-    }else{
-        console.log(`Connected to Database: ${MONGODB_URI}`)
-    }
-});
-
-// test if the database has connected successfully
-// let db = mongoose.connection;
-// db.once('open', ()=>{
-//     console.log('Database connected successfully')
-// })
-
-
-
+// Connecting to the database
+const MONGODB_URI = process.env.MONGODB_URI || config.mongoURI[app.settings.env];
+mongoose
+  .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log(`Connected to Database: ${MONGODB_URI}`);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 // View Engine
 app.set('view engine', 'ejs');
 
-// Set up the public folder;
-app.use(express.static(path.join(__dirname, 'public')));
+// Set up the public folder
+app.use(express.static(path.join(__dirname, 'public'));
 
-// body parser middleware
-app.use(express.json())
-
+// Body parser middleware
+app.use(express.json());
 
 app.use('/', index);
 app.use('/image', image);
 
-
-
- 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT,() =>{
-    console.log(`Server is listening at http://localhost:${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server is listening at http://localhost:${PORT}`);
 });
-
 
 module.exports = app;
